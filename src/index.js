10fed377a1352b33c43a5f19c57bfaee3c6157d4
crmcommit <sanketkumar38@gmail.com>
2021-06-18 12:49:00 +0530

    Event basic in react

--- src/index.js
+++ src/index.js
@@ -91,15 +91,34 @@ function BookList() {
 // always return value 
 // Object destructuring 
 const Book =(props) => {
+  // attribute , eventhandler
+  // onClick , onMouseOver
   // props destructing
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert('hello world');
+  }
+  const morecomplexample = (author) => {
+    console.log(author);
+  }
    const {image, title ,author,url} = props;
-   console.log(props);
   return (
     <div>
-      <article className="book">
+      {/* Mouse over */}
+      <article className="book" onMouseOver={() => {
+      console.log(title);
+      }}>
       <a href={url} target="_blank"> <img src={image}  alt={image} /></a>
-      <h1>{title}</h1>
+      <h1  onClick={ () => console.log(title)}>{title}</h1>
       <h4> {author}</h4>  
+       <button type="button" onClick={clickHandler}>
+         reference Example
+       </button>
+       {/* trigger only on click */}
+       <button type='button' onClick={() => morecomplexample(author)}>
+         More complex example
+       </button>
       </article>
   </div>
   );
