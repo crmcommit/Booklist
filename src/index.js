3d027ccdc1cd70223153e1d4dd40c3bc30ed0a77
crmcommit <sanketkumar38@gmail.com>
2021-06-18 12:13:40 +0530

    spread operator

--- src/index.js
+++ src/index.js
@@ -5,52 +5,61 @@ import ReactDom from 'react-dom'
 import './index.css'
 const books = [
    { 
+  id :1,  
   image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
   title : "Withyou",
   author: 'Akshay Kumar',
   url: "https://www.amazon.in/dp/9390791421/ref=sr_1_1?dchild=1&keywords=withyou&qid=1623952484&s=books&sr=1-1"
   },
   { 
+  id:2,
   image : "https://m.media-amazon.com/images/P/0143332023.01._SCLZZZZZZZ_SX500_.jpg",
   title : "Grandma's Bag of Stories",
   author: 'Sudha Murty',
   url : "https://www.amazon.in/Grandmas-Bag-Stories-Sudha-Murty/dp/0143333623/ref=zg_bs_books_13?_encoding=UTF8&psc=1&refRID=EVT31VRKAX7WBNBJ67AJ"
   },
   { 
+    id:3,
   image : "https://images-na.ssl-images-amazon.com/images/I/51eHb-R0PYL._SX381_BO1,204,203,200_.jpg",
   title : "Very First Sight Words Sentences Level",
   author: 'Ruskin Bond',
   url :"https://www.amazon.in/First-Sight-Words-Sentences-Level/dp/9387971996" 
   },
   { 
+    id:4,
   image : "https://images-na.ssl-images-amazon.com/images/I/410llGwMZGL._SX328_BO1,204,203,200_.jpg",
   title : "The Alchemist",
   author: 'Paul Coelho', 
   url : "https://www.amazon.in/Alchemist-Paulo-Coelho/dp/8172234988/ref=zg_bs_books_10?_encoding=UTF8&psc=1&refRID=EVT31VRKAX7WBNBJ67AJ"
   },
   { 
+    id:5,
   image : "https://m.media-amazon.com/images/P/0143332023.01._SCLZZZZZZZ_SX500_.jpg",
   title : "Grandma's Bag of Stories",
   author: 'Sudha Murty',
   url : "https://www.amazon.in/Grandmas-Bag-Stories-Sudha-Murty/dp/0143333623/ref=zg_bs_books_13?_encoding=UTF8&psc=1&refRID=EVT31VRKAX7WBNBJ67AJ"
   },
    { 
+     id:6,
   image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
   title : "Withyou",
   author: 'Akshay Kumar',
   url: "https://www.amazon.in/dp/9390791421/ref=sr_1_1?dchild=1&keywords=withyou&qid=1623952484&s=books&sr=1-1"
   },
   { 
+    id:7,
   image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
   title : "Withyou",
   author: 'Akshay Kumar'
   },
   { 
+    id:8,
   image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
   title : "Withyou",
   author: 'Akshay Kumar'
   },
   { 
+    id:9,
   image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
   title : "Withyou",
   author: 'Akshay Kumar'
@@ -62,16 +71,16 @@ function BookList() {
   // must return jsx
   return (
     <div>
-      <div class="header">
+      <div className="header">
   <h1>Best Sellers 2021</h1>
   <p>Booklist application built with React JS </p>
 </div>
      <Search/>
     <section className="booklist">
       {books.map((book) => { 
-        const {image,title, author, url} = book;
         return (
-         <Book book={book}></Book>
+          // spread operator to spread the object
+         <Book key={book.id} {...book}></Book>
         );
       })}
     </section>
@@ -82,9 +91,8 @@ function BookList() {
 // always return value 
 // Object destructuring 
 const Book =(props) => {
-  console.log(props);
   // props destructing
-   const {image, title ,author,url} = props.book;
+   const {image, title ,author,url} = props;
    console.log(props);
   return (
     <div>
