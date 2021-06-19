d53943b8dc6c7edcfeaa2c3304025d9aea185568
crmcommit <sanketkumar38@gmail.com>
2021-06-19 12:40:08 +0530

    updated dataset

--- src/books.js
+++ src/books.js
@@ -36,27 +36,30 @@ export const data = [
   },
    { 
      id:6,
-  image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
-  title : "Withyou",
-  author: 'Akshay Kumar',
-  url: "https://www.amazon.in/dp/9390791421/ref=sr_1_1?dchild=1&keywords=withyou&qid=1623952484&s=books&sr=1-1"
+  image : "https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg",
+  title : "The Psychology of Money",
+  author: 'Morgan Housel',
+  url: "https://www.amazon.in/Monk-Who-Sold-His-Ferrari/dp/817992162X/ref=zg_bs_books_36?_encoding=UTF8&psc=1&refRID=EX2ARR3NSHAEGKT2MB2G"
   },
   { 
     id:7,
-  image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
-  title : "Withyou",
-  author: 'Akshay Kumar'
+  image : "https://images-na.ssl-images-amazon.com/images/I/51H2M2jWmvL._SX311_BO1,204,203,200_.jpg",
+  title : "Great Stories for Children",
+  author: 'Ruskin bond',
+  url :"https://www.amazon.in/Great-Stories-Children-Ruskin-Bond/dp/8129118920/ref=sr_1_3?crid=538WJ1IDU9SW&dchild=1&keywords=great+stories+for+children+ruskin+bond&qid=1624086148&sprefix=great+sto%2Caps%2C373&sr=8-3"
   },
   { 
     id:8,
-  image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
-  title : "Withyou",
-  author: 'Akshay Kumar'
+  image : "https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+  title : "Atomic Habits",
+  author: 'James Clear',
+  url : "https://www.amazon.in/Psychology-Money-Morgan-Housel/dp/9390166268/ref=sr_1_1_sspa?crid=25RS8LMS43IZM&dchild=1&keywords=atomic+habit&qid=1624086127&sprefix=atomic+habi%2Caps%2C336&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzUU85TUkyMjdDTkc4JmVuY3J5cHRlZElkPUEwMTUwMDU4Mkc4VUNSUkZWSDFRSCZlbmNyeXB0ZWRBZElkPUEwMTQ1MTU4M0I4Mks1VTNZOVVXSCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
   { 
     id:9,
-  image : "https://images-na.ssl-images-amazon.com/images/I/41TtVWrPcaS._SX348_BO1,204,203,200_.jpg",
-  title : "Withyou",
-  author: 'Akshay Kumar'
+  image : "https://images-na.ssl-images-amazon.com/images/I/51wOOMQ+F3L._SX312_BO1,204,203,200_.jpg",
+  title : "Rich Dad Poor Dad",
+  author: 'Robert T. Kiyosaki',
+  url : "https://www.amazon.in/Rich-Dad-Poor-Middle-Updates/dp/1612680194/ref=sr_1_1?crid=Q1F43B1EOTK9&dchild=1&keywords=rich+dad+poor+dad+english&qid=1624086099&sprefix=rich+da%2Caps%2C399&sr=8-1"
   },
 ];
